b8844e187e9db0bc7dc8cd360e4a528f99c95fd7
Andrew <andrew-s@comcast.net>
2015-09-14 16:39:51 -0400

    Implemented Player.lua with 8 directions.

--- Player.lua
+++ Player.lua
@@ -1,1 +1,42 @@
+Player = Sprite:new(X,Y, ImageFile)
 
+Player.magnitude = 9
+Player.momentArm = math.sqrt(Player.magnitude^2/2)
+
+-- as of now you must use this method to change the magnitude
+-- otherwise, the momentArm will not be recalculated
+function Player:changeMagnitude(m)
+	self.magnitude = m
+	self.momentArm = math.sqrt(self.magnitude^2/2)
+end
+function Player:update()
+	if love.keyboard.isDown('w') and love.keyboard.isDown('d') then
+        	self.velocityX = self.momentArm
+		self.velocityY = -self.momentArm
+	elseif love.keyboard.isDown('d') and love.keyboard.isDown('s') then
+        	self.velocityX = self.momentArm
+		self.velocityY = self.momentArm
+	elseif love.keyboard.isDown('s') and love.keyboard.isDown('a') then
+        	self.velocityX = -self.momentArm
+		self.velocityY = self.momentArm
+	elseif love.keyboard.isDown('a') and love.keyboard.isDown('w') then
+        	self.velocityX = -self.momentArm
+		self.velocityY = -self.momentArm
+	elseif love.keyboard.isDown('w') then
+        	self.velocityX = 0
+		self.velocityY = -self.magnitude
+	elseif love.keyboard.isDown('s') then
+        	self.velocityX = 0
+		self.velocityY = self.magnitude
+	elseif love.keyboard.isDown('a') then
+        	self.velocityX = -self.magnitude
+		self.velocityY = 0
+	elseif love.keyboard.isDown('d') then
+        	self.velocityX = self.magnitude
+		self.velocityY = 0
+    	end
+	Sprite.update(self)
+end
+
+
+return Player
