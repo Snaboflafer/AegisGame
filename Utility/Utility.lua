ecc921bd1bee0adea2e09cacac9fc48803c27c3a
Steven Austin <sausti12@jhu.edu>
2015-10-26 19:13:17 -0400

    Valid key check for high score

--- Utility/Utility.lua
+++ Utility/Utility.lua
@@ -15,3 +15,29 @@ end
 function Utility:mid(Low, High)
 	return (High - Low) / 2
 end
+
+function Utility:isValidKey(key)
+	local validKeys = Utility:Set {
+						"a", "b", "c", "d",
+						"e", "f", "g", "h", 
+						"i", "j", "k", "l",
+						"o", "m", "n", "p",
+						"q", "r", "s", "t",
+						"u", "v", "w", "x", 
+						"y", "z", "0", "1",
+						"2", "3", "4", "5",
+						"6", "7", "8", "9"
+					}
+
+	if validKeys[key] == nil then
+		return false
+	else
+		return true
+	end
+end
+
+function Utility:Set(list)
+	local set = {}
+    for _, l in ipairs(list) do set[l] = true end
+    return set
+end
