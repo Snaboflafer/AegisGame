5d4e142adf60386173f60b71ae653aa1087984e6
Steven Austin <sausti12@jhu.edu>
2015-11-15 20:31:43 -0500

    Removed cutscene code

--- States/GameLoadState.lua
+++ States/GameLoadState.lua
@@ -7,14 +7,6 @@ setmetatable(GameLoadState, State)
 
 function GameLoadState:load()
 	State.load(self)
-
-	local currentLevel = General:getCurrentLevel()
-	local cutScene = Sprite:new(0, 0, LevelManager:getCutScene(currentLevel))
-
-	cutScene.scrollFactorX = 0
-	cutScene.scrollFactorY = 0
-
-	GameLoadState:add(cutScene)
 end
 
 function GameLoadState:start()
